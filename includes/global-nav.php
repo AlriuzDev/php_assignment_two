98bb353bd8880a08ebd521a15f592c7bf37fb5db
Anderson <avictoral@gmail.com>
2023-11-29 00:03:22 -0500

    updated global nav component

--- includes/global-nav.php
+++ includes/global-nav.php
@@ -2,10 +2,10 @@
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 <nav class="nav-container">
-    <section>
-        <!-- <i class='bx bxs-store-alt'></i> -->
+    <section class="nav-left">
+        <a class="dropdown-item" href="./includes/logout.php"><i class='bx bx-log-out'></i></a>
     </section>
-    <section>
+    <section class="nav-center">
         <h1><a class="navbar-brand" href="#"> Inven<span class="header-span">Tkr</span></a></h1>
     </section>
     <section class="nav-img">
@@ -13,7 +13,10 @@
         <?php
         session_start();
         ?>
-        <a class="dropdown-item" href="./includes/logout.php"><i class='bx bx-log-out'></i></a>
+        <div class="user-info">
+            <small> <?php echo $_SESSION['fullName'] ?></small>
+            <small><?php echo $_SESSION['role'] ?></small>
+        </div>
         <img src="<?php echo $_SESSION['avatar']; ?>" class="img-avatar" alt="...">
     </section>
 

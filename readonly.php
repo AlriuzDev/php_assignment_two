d86a055eedb7ed3d820df6143d14f1f5ad206c20
Anderson <avictoral@gmail.com>
2023-11-30 10:52:24 -0500

    updated readonly component

--- readonly.php
+++ readonly.php
@@ -14,9 +14,9 @@
     <title>InveTkr</title>
 </head>
 
-<body>
+<body class="body-product-info">
     <header>
-        <h1> Edit</h1>
+        <?php require './includes/global-nav.php'; ?>
     </header>
     <?php
 
@@ -36,56 +36,51 @@
         $pUpdated = $value['updatedAt'];
     }
     ?>
-    <section class="container">
-        <div class="row">
-            <div class="col-md-5 mx-auto">
-                <div class="card">
-                    <div class="card-header bg-primary">
-                        <h4 class="text-white">Update Records</h4>
+    <main class="main-product-info">
+        <section class="container">
+            <div class="main-dark-background" >
+                <h4 class="text-white">Update Records</h4>
+                <form method="POST" >
+
+                    <div class="form-group" style="margin-bottom: 1rem;">
+                        <label for="name">Title:</label>
+                        <input type="text" class="form-control" name="title" value="<?php echo $pTitle ?>" required style="background-color: #212529; color: #fbfbfb;" disabled>
                     </div>
-                    <div class="card-body bg-light">
-                        <form method="POST">
-
-                            <div class="form-group">
-                                <label for="name">Title:</label>
-                                <input type="text" class="form-control" name="title" value="<?php echo $pTitle ?>" required disabled>
-                            </div>
-                            <div class="form-group">
-                                <label for="price">Price:</label>
-                                <input type="text" class="form-control" name="price" value="<?php echo $pPrice ?>" required="" disabled>
-                            </div>
-                            <div class="form-group">
-                                <label for="description">Description:</label>
-                                <input type="text" class="form-control" name="description" value="<?php echo $pDescription ?>" required="" disabled>
-                            </div>
-                            <div class="form-group">
-                                <label for="images">images:</label>
-                                <img src="<?= $pImages1 ?>" alt="img1" class="img-fluid" disabled>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="createdAt">Created:</label>
-                                <input type="text" name="created" value="<?php echo $pCreated ?>" disabled>
-                            </div>
-                            <div class="form-group">
-                                <label for="updatedAt">Updated:</label>
-                                <input type="text" name="updated" value="<?php echo $pUpdated ?>" disabled>
-                            </div>
-                            <div class="form-group">
-                                <input type="hidden" name="productID" value="<?php echo $pID ?>">
-                                <a href="user-view.php" class="btn btn-primary"style="float:right;">
-                                   Return
-                                </a>
-                                <!-- <input type="submit" name="return" class="btn btn-primary" style="float:right;" value="Return"> -->
-                            </div>
-
-                        </form>
-
+                    <div class="form-group" style="margin-bottom: 1rem;">
+                        <label for="price">Price:</label>
+                        <input type="text" class="form-control" name="price" value="<?php echo $pPrice ?>" required="" style="background-color: #212529; color: #fbfbfb;" disabled>
                     </div>
-                </div>
+                    <div class="form-group" style="margin-bottom: 1rem;">
+                        <label for="description">Description:</label>
+                        <textarea type="text" class="form-control" name="description" required="" style="background-color: #212529; color: #fbfbfb;" disabled><?php echo $pDescription ?></textarea>
+                    </div>
+                    <div class="form-group" style="margin-bottom: 1rem;">
+                        <label for="images">images:</label>
+                        <img src="<?= $pImages1 ?>" alt="img1" class="img-fluid" style="background-color: #212529; color: #fbfbfb;" disabled>
+                    </div>
+
+                    <div class="form-group" style="margin-bottom: 1rem;">
+                        <label for="createdAt">Created:</label>
+                        <input type="text" name="created" value="<?php echo $pCreated ?>" style="background-color: #212529; color: #fbfbfb;"  disabled>
+                    </div>
+                    <div class="form-group" style="margin-bottom: 1rem;">
+                        <label for="updatedAt">Updated:</label>
+                        <input type="text" name="updated" value="<?php echo $pUpdated ?>" style="background-color: #212529; color: #fbfbfb;" disabled>
+                    </div>
+                    <div class="form-group">
+                        <input type="hidden" name="productID" value="<?php echo $pID ?>">
+                        <a href="user-view.php" class="btn btn-primary" style="float:right;">
+                            Return
+                        </a>
+                    </div>
+
+                </form>
+
             </div>
-        </div>
-    </section>
+        </section>
+
+    </main>
+
 </body>
 
 </html>
